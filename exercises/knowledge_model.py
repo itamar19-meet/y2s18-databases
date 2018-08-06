23901529fbb8a3690c6a56656b976ac4e635a3f5
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 10:24:08 +0300

    badgb

--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -13,5 +13,20 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	__tablename__ = 'students'
+	ID = Column(Integer, primary_key=True)
+	article_name = Column(String)
+	topic = Column(String)
+	rating = Column(Integer)
 
-	pass
+	def __repr__(self):
+		return ("article_name: {}\n"
+				"topic of article: {}\n"
+				"rating of article: {}\n"
+	 			"ID: {}").format(
+					self.article_name,
+					self.topic,
+					self.rating,
+					self.ID)
+a = Knowledge(article_name ="dff",topic ="cars", rating = 5)
+a.Knowledge()
